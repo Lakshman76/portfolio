d5a2b4004b6ed7fe43d91aa525d89c6019fb25d7
Lakshman Kumar <lakshmankumar2603@gmail.com>
2024-09-06 13:30:19 +0530

    Develop experience section

--- src/constants/index.js
+++ src/constants/index.js
@@ -129,7 +129,7 @@ import {
       ],
     },
     {
-      title: "React Native Developer",
+      title: "React Developer",
       company_name: "Tesla",
       icon: tesla,
       iconBg: "#E6DEDD",
